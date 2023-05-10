35f56a9e3d4ab7af8a2d0e11eb72f397806d5a0f
b22samer <b22samer@student.his.se>
2023-05-10 10:16:30 +0200

    Jag gör så att man kan skicka datan från Activity2.java till MainActivity.java

--- app/src/main/java/com/example/project/Activity2.java
+++ app/src/main/java/com/example/project/Activity2.java
@@ -1,13 +1,42 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+
+    private EditText skrivText;
+    private Button button;
+    private String sendText;
+    SharedPreferences myPreferenceRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        skrivText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        myPreferenceRef = getSharedPreferences("pref", MODE_PRIVATE);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = myPreferenceRef.edit();
+                sendText = skrivText.getText().toString();
+                editor.putString("name", sendText);
+                editor.apply();
+                Log.d("==>", "going back");
+                Intent intent = new Intent(Activity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,35 +14,30 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private SharedPreferences myPreferenceRef;
-    private SharedPreferences.Editor myPreferenceEditor;
+    private TextView prefText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        myPreferenceRef = getPreferences(MODE_PRIVATE);
-        myPreferenceEditor = myPreferenceRef.edit();
-        TextView prefTextRef=new TextView(this);
-        prefTextRef=(TextView)findViewById(R.id.prefText);
-        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
-
-        button = (Button) findViewById(R.id.button);
+        prefText = findViewById(R.id.prefText);
+        myPreferenceRef = getSharedPreferences("pref", MODE_PRIVATE);
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Activity2.class);
-
                 startActivity(intent);
-
-
             }
         });
     }
-
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name = myPreferenceRef.getString("name", "no name was found");
+        prefText.setText(name);
+    }
 }
-
